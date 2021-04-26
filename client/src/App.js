cc45247850d6a5e894de889b383fb3b78efb9bee
Darío Fernández <dariofedes@protonmail.com>
2021-04-26 14:27:14 +0200

    adding Home page to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { Header } from './components';
+import { Home } from './pages'
 
 
 function App() {
   return (
     <div className="App">
       <Header title="List-It!" />
+      <Home />
     </div>
   );
 }
