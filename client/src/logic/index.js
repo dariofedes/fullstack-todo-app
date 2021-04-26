ca52b6ea846cba2fdee5f5f77e88a5da09d026a0
Darío Fernández <dariofedes@protonmail.com>
2021-04-26 14:26:00 +0200

    exporting logics

---
+++ client/src/logic/index.js
@@ -0,0 +1,9 @@
+import retrieveTasks from './retrieve-tasks'
+import saveTask from './save-task'
+import deleteTask from './delete-task'
+
+export {
+    retrieveTasks,
+    saveTask,
+    deleteTask,
+}
