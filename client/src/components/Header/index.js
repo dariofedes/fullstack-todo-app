9d05f9697e04aa0906d2f265eee3b477e709906e
Darío Fernández <dariofedes@protonmail.com>
2021-04-26 14:01:33 +0200

    adding propTypes to Header component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 
 const Header = ({ title }) => {
   return (
@@ -6,6 +8,10 @@ const Header = ({ title }) => {
       <h1 className="header__title">{title}</h1>
     </header>
   )
+}
+
+Header.propTypes = {
+  title: PropTypes.string
 }
 
 export default Header
